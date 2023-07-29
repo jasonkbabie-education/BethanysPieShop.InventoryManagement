256108adb03992c63d9025e70e8dddfd0aae9760
Jason K. Babie <jasonkbabie@outlook.com>
2023-07-29 11:36:25 +0200

    Add Implementation Methods for Product Class

--- BethanysPieShop.InventoryManagement/Product.cs
+++ BethanysPieShop.InventoryManagement/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,5 +18,72 @@ namespace BethanysPieShop.InventoryManagement
         private UnitType unitType;
         private int amountInStock = 0;
         private bool isBelowStockThreshold = false;
+
+        public void UseProduct(int items)
+        {
+            if (items <= amountInStock)
+            {
+                amountInStock -= items;
+                UpdateLowStock();
+                Log($"Amount in stock updated. Now {amountInStock} items in stock");
+            }
+            else
+            {
+                Log($"Not enough items on stock for {CreateSimpleProductRepresentation()}. {amountInStock} available but {items} requested.");
+            }
+        }
+
+        public void IncreaseStock()
+        {
+            amountInStock++;
+        }
+
+        private void DecreaseStock(int items, string reason)
+        {
+            if (items <= amountInStock)
+            {
+                amountInStock -= items;
+            }
+            else
+            {
+                amountInStock = 0;
+            }
+            UpdateLowStock();
+            Log(reason);
+        }
+
+        public string DisplayDetailsShort()
+        {
+            return $"{id}. {name} \n{amountInStock} items in stock";
+        }
+
+        public string DisplayDetailsFull()
+        {
+            StringBuilder sb = new();
+            sb.Append($"{id} {name}\n{description}\n{amountInStock} items(s) in stock.");
+
+            if (isBelowStockThreshold)
+            {
+                sb.Append("\n!!STOCK LOW!!");
+            }
+            return sb.ToString();
+        }
+        private object CreateSimpleProductRepresentation()
+        {
+            return $"Product {id} ({name})";
+        }
+
+        private void Log(string message)
+        {
+            Console.WriteLine(message);
+        }   
+
+        private void UpdateLowStock()
+        {
+            if (amountInStock < 10)
+            {
+                isBelowStockThreshold = true;
+            }
+        }
     }
 }
